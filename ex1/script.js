6a0dfe2f1b3d45912c510fff27e6ae1a922d8429
Deivid Rocha <deividzor@gmail.com>
2026-04-29 22:22:55 -0300

    string exercicios

--- ex1/script.js
+++ ex1/script.js
@@ -1,5 +1,12 @@
-const nome = prompt('Digite seu nome completo: ');
-document.body.innerHTML = `O seu nome é ${nome} <br />`
 
 
-
+  const nome = prompt('Digite seu nome completo:');
+  document.body.innerHTML += `Seu nome é: ${nome}<br />`;
+  document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
+  document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br />`;
+  document.body.innerHTML += `Qual o primeiro índice da letra E no seu nome? ${nome.toUpperCase().indexOf("E")}<br />`;
+  document.body.innerHTML += `Qual o último índice da letra LETRA no seu nome? ${nome.toUpperCase().lastIndexOf("D")}<br />`;
+  document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
+  document.body.innerHTML += `As palavras do seu nome são: ${nome.split(" ")}<br />`;
+  document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br />`;
+  document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br />`;
